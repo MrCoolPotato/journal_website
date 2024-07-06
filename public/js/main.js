cac479c14425e6225964cebbd2622cc59b18dd92
Axel <akselpekin@me.com>
2024-07-06 21:45:45 +0300

    new style

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,30 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const articlesContainer = document.getElementById("articles-container");
-  const res = await fetch("/api/articles");
+  const latestArticleContainer = document.getElementById("latest-article");
+  const otherArticlesContainer = document.getElementById("other-articles");
+
+  // Fetch dummy data
+  const res = await fetch("dummy-data.json");
   const articles = await res.json();
 
-  articles.forEach((article) => {
-    const articleDiv = document.createElement("div");
-    articleDiv.className = "article";
-    articleDiv.innerHTML = `
-            <h3>${article.title}</h3>
-            <p>${article.content.substring(0, 100)}...</p>
-            <a href="/article.html?id=${article._id}">Read more</a>
+  if (articles.length > 0) {
+    const latestArticle = articles[0];
+    latestArticleContainer.innerHTML = `
+            <img src="images/sample.jpg" alt="Article Image">
+            <h2>${latestArticle.title}</h2>
+            <p>${latestArticle.content.substring(0, 200)}...</p>
+            <a href="/article.html?id=${latestArticle.id}">Read more</a>
         `;
-    articlesContainer.appendChild(articleDiv);
-  });
+
+    articles.slice(1).forEach((article) => {
+      const articleDiv = document.createElement("div");
+      articleDiv.className = "article";
+      articleDiv.innerHTML = `
+                <img src="images/sample.jpg" alt="Article Image">
+                <h3>${article.title}</h3>
+                <p>${article.content.substring(0, 100)}...</p>
+                <a href="/article.html?id=${article.id}">Read more</a>
+            `;
+      otherArticlesContainer.appendChild(articleDiv);
+    });
+  }
 });
